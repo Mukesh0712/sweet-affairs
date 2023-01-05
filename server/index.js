9be6a629c25338a2565c7a66bff1b45927a1b4ef
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2023-01-05 22:26:38 +0530

    feat:signup validation

--- server/index.js
+++ server/index.js
@@ -20,6 +20,42 @@ app.post('/signup', async (req, res)=>{
 
     const{name, phone, email, password, role} = req.body;
 
+    //validation to check all fields are filled
+    const emptyFields = [];
+
+    if(!name) emptyFields.push('name');
+    if(!phone) emptyFields.push('phone');
+    if(!email) emptyFields.push('email');
+    if(!password) emptyFields.push('password');
+    if(!role) emptyFields.push('role');
+
+    if(emptyFields.length>0){
+        return res.json({
+            success:false,
+            message: `${emptyFields.join(', ')} are required`
+        })
+    }
+
+    //validation to check if email already exists
+    const existingUserEmail = await User.findOne({email: email})
+
+    if(existingUserEmail){
+        return res.json({
+            success: false,
+            message: `Email ID ${email} Already Registered`
+        })
+    }
+
+    //validation to check if phone already exists
+    const existingUserPhone = await User.findOne({phone: phone})
+
+    if(existingUserPhone){
+        return res.json({
+            success: false,
+            message: `Phone Number ${phone} Already Registered`
+        })
+    }
+
     const user = new User({
         name: name,
         phone: phone,
@@ -35,8 +71,10 @@ app.post('/signup', async (req, res)=>{
         message: `${name} you have Registered Successfully`,
         data: savedUser
     })
-    
+
 })
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server Running on Port ${PORT}`);
