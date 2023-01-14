15a9cad46b1911504808e1e16ce75f2203d535d3
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2023-01-14 19:01:46 +0530

    feat: add to plate done

--- client/src/components/FoodItemCard/FoodItemCard.js
+++ client/src/components/FoodItemCard/FoodItemCard.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./FoodItemCard.css"
 
 function FoodItemCard({ category, description, imgURL, price, title }) {
+
+    const [quantity, setQuantity] = useState(1)
+
+    async function addToPlate(){
+        const plateObject = {
+            name: title,
+            price: price,
+            quantity: quantity
+        }
+
+        //add to plate in localStorage
+
+        const existingPlate = JSON.parse(localStorage.getItem('plate')) || []
+
+        existingPlate.push(plateObject)
+
+        localStorage.setItem('plate', JSON.stringify(existingPlate))
+    }
+
     return (
         <div className='col-md-3'>
             <div className='food-item-card'>
@@ -11,8 +30,17 @@ function FoodItemCard({ category, description, imgURL, price, title }) {
                 </div>
                 <h2>{title}</h2>
                 <p>{description}</p>
-                <p>{price}</p>
-                <p>{category}</p>
+                <p>₹ {price} /- Only</p>
+                <span>{category}</span>
+
+                <div>
+                    <span onClick={(e)=>{setQuantity(quantity - 1)}}>-</span> 
+                    <h2>{quantity}</h2> 
+                    <span onClick={(e)=>{setQuantity(quantity + 1)}}>+</span>
+                </div>
+            </div>
+            <div className='text-center'>
+                <button type='button' className='btn-add-to-plate' onClick={addToPlate}>Add To Plate</button>
             </div>
         </div>
     )
