b50f278aece61e7da1f7eb85a38ec26a65a4810b
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2023-01-14 16:30:33 +0530

    feat: util added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -4,10 +4,21 @@ import "./Home.css"
 
 function Home() {
 
+    function logOut(){
+        localStorage.removeItem('currentUser')
+        window.location.href = '/login'
+    }
+
+    if(!currentUser){
+        window.location.href = '/login'
+    }
+
     return (
         <div>
             <h1 className='text-center'>Home</h1>
             <h2>{currentUser?.name}</h2>
+
+            <button type='button' className='btn btn-danger' onClick={logOut}>Logout</button>
         </div>
     )
 }
