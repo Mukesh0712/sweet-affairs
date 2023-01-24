563fb47c4b6cb60b07d3f75728c3805d98065c17
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2023-01-24 10:14:53 +0530

    feat: orderfooditems api call and function

--- client/src/views/MyPlate/MyPlate.js
+++ client/src/views/MyPlate/MyPlate.js
@@ -1,41 +1,63 @@
 import React from 'react'
+import axios from 'axios'
+import swal from 'sweetalert'
 import "./MyPlate.css"
 import Navbar from '../../components/Navbar/Navbar'
+import { currentUser } from '../../util/currentUser'
 import { myFoodPlateItems } from "./../../util/myPlate"
 
 function MyPlate() {
+
+    async function placeFoodOrder() {
+        const response = await axios.post("/orderFoodItems", {
+            userID: currentUser.id,
+            tableNumber: localStorage.getItem("tableNumber") || 1,
+            items: myFoodPlateItems
+        })
+
+        if (response.data.success) {
+            await swal("Order Placed", response.data.message, "success")
+            localStorage.removeItem("plate")
+            window.location.href = "/"
+        }
+    }
+
     return (
         <div>
             <Navbar />
             <h2 className='text-center'>My Plate 🍽️</h2>
-            {
-                myFoodPlateItems.map((item, index) => {
-                    return (
-                        <div className='container'>
+            <div className='my-plate-container row'>
+                {
+                    myFoodPlateItems.map((item, index) => {
 
-                            <div className='plate-container'>
+                        return (
+                            <div className='container col-md-4'>
 
-                                <div className='plate'>
-                                    <h6 className='name'>Name : {item.name}</h6>
-                                </div>
+                                <div className='plate-container'>
 
-                                <div className='plate'>
-                                    <h6 className='price'>Price : {item.price}</h6>
-                                </div>
+                                    <div className='plate'>
+                                        <h6 className='name'>Name : {item.name}</h6>
+                                    </div>
 
-                                <div className='plate'>
-                                    <h6 className='quantity'>Quantity : {item.quantity}</h6>
+                                    <div className='plate'>
+                                        <h6 className='price'>Price : {item.price}</h6>
+                                    </div>
+
+                                    <div className='plate'>
+                                        <h6 className='quantity'>Quantity : {item.quantity}</h6>
+                                    </div>
+
                                 </div>
 
                             </div>
-
-                        </div>
-                    )
-                })
-            }
+                        )
+                        
+                    })
+                }
+            </div>
 
             <div className='confirm-btn'>
-                <button className='btn btn-warning fs-5 fw-bold button'>Confirm Order</button>
+                <button className='btn btn-warning fs-5 fw-bold button' onClick={placeFoodOrder}>Confirm Order</button>
             </div>
 
         </div>
