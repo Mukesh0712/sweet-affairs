8f80f2c9d84e803196cee7d882368c672ac0bf7b
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2023-01-28 22:23:33 +0530

    feat: navbar hover effect and logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import "./Navbar.css"
 import { currentUser } from '../../util/currentUser'
 import { myFoodPlateCount } from '../../util/myPlate'
 import { Link } from "react-router-dom"
+import sweetLogo from "./../../assets/rest-name-logo.png"
 
 function Navbar({ user }) {
 
@@ -30,7 +31,7 @@ function Navbar({ user }) {
 
                 <div class="container-fluid">
 
-                    <Link class="navbar-brand ms-2" to="/">SWEET AFFAIRS</Link>
+                    <img class="navbar-brand ms-2 rest-name" alt='rest-name' src={sweetLogo} />
 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -38,41 +39,41 @@ function Navbar({ user }) {
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                        <ul class="navbar-nav me-0 mb-2 mt-2 mb-lg-0 ms-auto pt-2 ">
+                        <ul class="navbar-nav me-0 mb-2 mt-2 mb-lg-0 ms-auto pt-2">
 
                             <li class="nav-item me-4">
-                                <Link class="nav-link active" aria-current="page" to="/">HOME</Link>
+                                <Link class="nav-link link-hover fw-bold" aria-current="page" to="/">HOME</Link>
                             </li>
 
                             <li class="nav-item me-4">
-                                <Link class="nav-link active" to="/">ABOUT</Link>
+                                <Link class="nav-link link-hover fw-bold" to="/">ABOUT</Link>
                             </li>
 
                             <li class="nav-item me-4">
-                                <Link class="nav-link active" to='/menu' role="button" >MENU</Link>
+                                <Link class="nav-link link-hover fw-bold" to='/menu' role="button" >MENU</Link>
                             </li>
 
                             <li class="nav-item me-4">
-                                <Link class="nav-link active" to="/tables">TABLES</Link>
+                                <Link class="nav-link link-hover fw-bold" to="/tables">TABLES</Link>
                             </li>
 
                             <li class="nav-item me-4">
-                                <Link class="nav-link active" to="/">CONTACT</Link>
+                                <Link class="nav-link link-hover fw-bold" to="/">CONTACT</Link>
                             </li>
 
                             <li class="nav-item me-4">
-                                <p className='username nav-link'>Welcome, {currentUser?.name}</p>
+                                <p className='username nav-link link-hover border-0 fw-bold'>Welcome, {currentUser?.name}</p>
                             </li>
 
                             <li class="nav-item me-4">
-                                <button type='button' className='btn logout-btn text-white fw-bold' onClick={logOut}>LOG OUT</button>
+                                <button type='button' className='btn logout-btn text-white fw-bold border-0 link-hover' onClick={logOut}>LOG OUT</button>
                             </li>
 
                             <li class="nav-item me-4">
                                 <button type="button" class="btn position-relative">
-                                    <Link to="/myPlate">
-                                        <i class="fa-solid fa-cart-shopping text-black"></i>
-                                        <span class="nav-link active position-absolute mt-2 top-0 start-100 translate-middle badge rounded-pill badge-bg text-white fw-bold">{foodItemCount}</span>
+                                    <Link to="/myPlate fw-bold ">
+                                        <i class="fa-solid fa-cart-shopping text-black link-hover border-0"></i>
+                                        <span class="nav-link position-absolute mt-2 top-0 start-100 translate-middle badge rounded-pill badge-bg text-white fw-bold">{foodItemCount}</span>
                                     </Link>
                                 </button>
                             </li>
