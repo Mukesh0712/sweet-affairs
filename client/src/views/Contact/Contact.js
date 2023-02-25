86cb3f2226a610ad62faafb35b153ad878f1bec9
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2023-02-25 20:27:14 +0530

    contact page design done

--- client/src/views/Contact/Contact.js
+++ client/src/views/Contact/Contact.js
@@ -37,7 +37,7 @@ function Contact() {
 
                     <div className='col-6 map-container'>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29815.897802405478!2d79.00857360714723!3d20.91283361684357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1677257608188!5m2!1sen!2sin"
-                            className='map' height="299px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">\
+                            className='map' height="319px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">\
                         </iframe>
                     </div>
 
